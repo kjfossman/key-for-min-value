579f1bee9a4f982d99d1b9809fadddb450b2a707
Kyle <kjfossman@gmail.com>
2021-02-04 18:01:20 -0900

    commit tree 2fb6af8e920dc51aa60e6eb1ac11afca75a16404

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    array = name_hash.collect do |key, value|
+        value
+    end
+    test = array.min 
+    name_hash.each do |key, value|
+        return key if value == test 
+       
+    end
+    nil
 end
